5a9632821d2386f1f4f4d35083e184033f0455bd
Yi <yiyang0325@gmail.com>
2015-12-16 18:27:09 -0800

    set up first view url and admin

--- article/views.py
+++ article/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Article
 # Create your views here.
+
+def allArticles(request):
+	articles = Article.objects.all()
+
+	context = {
+		'articles': articles,
+	}
+	return render(request, "home.html", context)
